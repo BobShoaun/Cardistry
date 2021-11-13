4d97aebbf5dc4ec532fc7abb0adc630b05c43807
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-11-13 17:58:07 -0500

    setup express server

---
+++ cardistry_example/public/script.js
@@ -0,0 +1,3 @@
+"use strict";
+
+console.log("Loaded cardistry from server");
---
+++ cardistry_example/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.use(express.static("public"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Server listening on port: ", port);
+});
